a15a4e4eece4b212a1a9190f564e2954db6af792
joshuafarrell95 <joshfarrell88@gmail.com>
2022-09-14 10:00:32 +0800

    Add project files.

---
+++ FormWikiApplication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WikiApplication_AT2
+{
+    public partial class FormWikiApplication : Form
+    {
+        public FormWikiApplication()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Information.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiApplication_AT2
+{
+    internal class Information
+    {
+        private string name;
+        private string category;
+        private string structure;
+        private string definition;
+
+        #region Setters and Getters
+        public void SetName(string newName)
+        {
+            name = newName;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetCategory(string newCategory)
+        {
+            category = newCategory;
+        }
+
+        public string GetCategory()
+        {
+            return category;
+        }
+
+        public void SetStructure(string newStructure)
+        {
+            structure = newStructure;
+        }
+
+        public string GetStructure()
+        {
+            return structure;
+        }
+
+        public void SetDefinition(string newDefinition)
+        {
+            definition = newDefinition;
+        }
+
+        public string GetDefinition()
+        {
+            return definition;
+        }
+        #endregion
+    }
+}
